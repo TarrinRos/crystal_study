d234e323f3732aca208c947388b94361519cfdde
TarrinRos <tarindis@gmail.com>
2019-02-09 22:54:55 +0200

    Error2 and veriables

---
+++ error2.cr
@@ -0,0 +1,7 @@
+str = "Crystal"
+
+ix = str.index('z')
+
+if ix
+  puts str[ix]
+end
---
+++ variables.cr
@@ -0,0 +1,7 @@
+name = "Diamond"
+element = 'C'
+hardness = 10
+
+puts typeof(name)
+puts typeof(element)
+puts typeof(hardness)
